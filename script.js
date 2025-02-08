1719892bcb3624b437bc7b33c40658fbac09d1e4
chathurahansaka1 <hansakachathura4@gmail.com>
2025-02-08 10:55:47 +0530

    Update script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const loadingScreen = document.getElementById('loading-screen');
-    
-    // Hide loading screen after 2 seconds
-    setTimeout(() => {
-        loadingScreen.style.display = 'none';
-    }, 2000);
-});
